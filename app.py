c4987b574b6dfdda02056b1737460f4ff366d0d8
Shimshon Feder <s0583227649@gmail.com>
2026-01-26 14:41:49 +0200

    Instance of space net and satellite

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from space_network_lib import SpaceEntity, Packet
+from space_network_lib import SpaceEntity, Packet, SpaceNetwork
 
 
 class Satellite(SpaceEntity):
 
     def receive_signal(self, packet: Packet):
         print(f"[{self.name}] Received: {packet}")
+
+# Instance for a spce network for transmitting massages
+space_net_1 = SpaceNetwork(level=1)
+
+# Two instance of sate llite
+sat_1  = Satellite("Sat1", 100)
+sat_2  = Satellite("Sat2", 200)
