24244c3e6ff279ba2dde584b14cff8e6490789b8
Shimshon Feder <s0583227649@gmail.com>
2026-01-26 14:46:30 +0200

    create massage, sent with space net

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from space_network_lib import SpaceEntity, Packet, SpaceNetwork
 
-
+# class of sate llite, inherits from SpaceEntity
 class Satellite(SpaceEntity):
 
     def receive_signal(self, packet: Packet):
@@ -12,3 +12,9 @@ space_net_1 = SpaceNetwork(level=1)
 # Two instance of sate llite
 sat_1  = Satellite("Sat1", 100)
 sat_2  = Satellite("Sat2", 200)
+
+# Create a massage(packet)
+msg_1 = Packet("Hello from spaces!", sat_1, sat_2)
+
+# Send message with our space network
+space_net_1.send(msg_1)
